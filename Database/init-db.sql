ba053b092b87154e35d580cd9ac398f68c43ed9e
Cezar D <cezar.dimoiu@gmail.com>
2021-12-26 18:06:23 +0200

    modified database structure

--- Database/init-db.sql
+++ Database/init-db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS medicine (
     id serial PRIMARY KEY,
-    med_name varchar2 NOT NULL,
-    quantity number NOT NULL,
-    med_description varchar2
+    med_name varchar NOT NULL,
+    quantity integer NOT NULL,
+    med_description varchar
 );
